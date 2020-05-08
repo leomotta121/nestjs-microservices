ec70f6835f2ef3cfaf2f3172102d305771a5ff09
Leonardo Motta <leonardo.nogueira@coopersystem.com.br>
2020-05-08 13:59:34 -0300

    feat: add math TCP transporter

--- mathservice/src/app.controller.ts
+++ mathservice/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
+  private logger = new Logger('AppController');
+
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('add')
+  add(data: number[]): number {
+    this.logger.log('Adding ' + data.toString());
+
+    return this.appService.accumulate(data);
   }
 }
--- mathservice/src/app.service.ts
+++ mathservice/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  accumulate(data: number[]): number {
+    return (data || []).reduce((a, b) => Number(a) + Number(b));
   }
 }
